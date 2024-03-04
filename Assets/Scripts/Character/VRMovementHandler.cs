05146397790fe14860fcd6e1230065e4371b2cc9
Amirali <amirali.mnj@gmail.com>
2024-03-04 12:33:06 +0000

    Add Character height set

---
+++ Assets/Scripts/Character/VRMovementHandler.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using UnityEngine;
+
+namespace Character
+{
+    public class VRMovementHandler : MonoBehaviour
+    {
+        public float standingHeight = 1.822f;
+        public float crouchingHeight;
+        public bool isStanding = true;
+        public Transform vrHeadTransform;
+        public LayerMask canStandLayers;
+
+        public float headSyncSmooth = 100;
+
+
+        public float GetCurrentHeight()
+        {
+            var ray = new Ray(vrHeadTransform.position, Vector3.down);
+            var hits = new RaycastHit[5];
+            var hitCount = Physics.RaycastNonAlloc(ray, hits, standingHeight + 3f, canStandLayers);
+            if (hitCount > 0)
+            {
+                Debug.DrawRay(vrHeadTransform.position, Vector3.down *  hits[0].distance);
+                return hits[0].distance;
+            }
+            else
+            {
+                Debug.LogError("PLAYER IS IN THE AIR!");
+                return -1f;
+            }
+        }
+        private void FixedUpdate()
+        {
+            var currentHeight = GetCurrentHeight(); 
+            if (isStanding && currentHeight > 0)
+            {
+                var vrHeadCurrentPos = vrHeadTransform.position;
+                if (!Mathf.Approximately(currentHeight, standingHeight))
+                {
+                    var targetPos = new Vector3(vrHeadCurrentPos.x, (vrHeadCurrentPos.y-currentHeight) +  standingHeight  , vrHeadCurrentPos.z);
+                    Debug.LogWarning("Dist:" +  (currentHeight) + " Target: " + standingHeight);
+                    vrHeadTransform.position =  Vector3.Lerp(vrHeadCurrentPos, targetPos, Time.deltaTime * headSyncSmooth);
+                }
+            }
+        }
+    }
+}
